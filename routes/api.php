9f7d72a93fd329046e0bdcd2d1c7a1719851c690
Williamdarmawan11106 <william.darmaw4n@gmail.com>
2024-05-04 15:12:42 +0700

    API DONE

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\PresensiController;
-use App\Http\Controllers\PemesananController;
+use App\Http\Controllers\DetailPesananController;
 
 Route::apiResource('customer', CustomerController::class);
 Route::apiResource('presensi', PresensiController::class);
-Route::apiResource('pemesanan', PemesananController::class);
+Route::apiResource('detailPesanan', DetailPesananController::class);
+// Route::get('detailPesanan/searchByNamaProduk/{namaProduk}', [DetailPesananController::class, 'searchByNamaProduk'])->name('detailPesanan.searchByNamaProduk');
+
+Route::get('/detailPesanan/search/{nama}', [DetailPesananController::class, 'searchByNamaProduk']);
+Route::get('/detailPesanan', [DetailPesananController::class, 'getAllDetailPesanan']);
